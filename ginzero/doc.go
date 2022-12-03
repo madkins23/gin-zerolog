dd60a8484af5aceee57498a8ef6a853a69c7b6b0
mAdkins <software@doorways.org>
2022-12-03 10:33:12 -0800

    Deprecate ginzero package

--- ginzero/doc.go
+++ ginzero/doc.go
@@ -2,6 +2,8 @@
 // This includes the ginzero.Logger [gin] Middleware to dump request data into [zerolog] and
 // the ginzero.Writer `IO.Writer` to trap low-level [gin] messaging.
 //
+// Deprecated: gin-zerolog/ginzero has been replaced by gin-utils/ginzero
+//
 // [gin]: https://gin-gonic.com/docs/
 // [zerolog]: https://github.com/rs/zerolog
 package ginzero
